28905884bffd973c2032f21347cbc231f2f3f5a6
AlexanderWoropai <71937498+AlexanderWoropai@users.noreply.github.com>
2022-03-23 14:07:27 +0300

    Create and edit tests for test bonuses

--- TestsForProgramm/UnitTest1.cs
+++ TestsForProgramm/UnitTest1.cs
@@ -15,10 +15,22 @@ namespace TestsForProgramm
         }
 
         [Test]
-        public void UsualStatementTest()
+        public void UsualStatementTestWithBonuses()
         {
             var SomeBill = new Bill(new Customer("Test", 10));
             var expected = "Счет для Test\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tCola\t\t65\t6\t390\t21,7\t368,3\t19\n\tPepsi\t\t50\t3\t150\t0\t150\t1\n\tFanta\t\t35\t1\t35\t0\t35\t0\nСумма счета составляет 553,3\nВы заработали 20 бонусных балов";
+            SomeBill.addGoods(new Item(new Goods("Cola", 0), 6, 65));
+            SomeBill.addGoods(new Item(new Goods("Pepsi", 1), 3, 50));
+            SomeBill.addGoods(new Item(new Goods("Fanta", 2), 1, 35));
+            var actual = SomeBill.statement();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void UsualStatementTestWithoutBonuses() 
+        {
+            var SomeBill = new Bill(new Customer("Test", 0));
+            var expected = "Счет для Test\n\tНазвание\tЦена\tКол-воСтоимость\tСкидка\tСумма\tБонус\n\tCola\t\t65\t6\t390\t11,7\t378,3\t19\n\tPepsi\t\t50\t3\t150\t0\t150\t1\n\tFanta\t\t35\t1\t35\t0\t35\t0\nСумма счета составляет 563,3\nВы заработали 20 бонусных балов";
             SomeBill.addGoods(new Item(new Goods("Cola", 0), 6, 65));
             SomeBill.addGoods(new Item(new Goods("Pepsi", 1), 3, 50));
             SomeBill.addGoods(new Item(new Goods("Fanta", 2), 1, 35));
